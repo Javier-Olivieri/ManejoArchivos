dd0ea569a9df09123232988782467e224c2b2771
Javier Olivieri <cosoligroup@gmail.com>
2022-02-23 16:33:54 -0300

    Archivos

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,141 +1,88 @@
-const fs = require("fs");
+const fs = require('fs')
 
-const objProducto1 = {
-  title: "Escuadras",
-  price: 123.5,
-  thumbnail:
-    "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-};
-const objProducto2 = {
-  title: "Calculadora",
-  price: 123.45,
-  thumbnail:
-    "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-};
-const objProducto3 = {
-  title: "Globo T.",
-  price: 123.45,
-  thumbnail:
-    "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-};
+
+
 
 class Contenedor {
-  constructor(archivo) {
-    this.archivo = archivo;
-    this.datos = [];
-    this.id = 0;
-  }
-  async save(producto) {
-    
-    await this.getAll();
-    this.id++;
-    this.datos.push({ ...producto, id: this.id });
-    try {
-
-      await fs.promises.writeFile(
-        this.archivo,
-        JSON.stringify(this.datos, null, 2)
-      );
-    } catch (error) {
-      console.log("No se pudo guardar el producto" + error);
+    constructor() {
+        this.id = undefined,
+            this.title = undefined,
+            this.price = undefined,
+            this.thumbnail = undefined
     }
-  }
 
-  async getById(id) {
-   
-    try {
-      if (this.datos) {
-     const pd = this.datos.find((prod) => prod.id === id);
-     
-        console.log("Producto encontrado:\n ");
-        console.log(pd);
-      } else {
-        console.log(`No se encuentra el producto con id: ${id}`);
-      }
-    } catch (error) {
-      console.log(error);
+    save = (title, price, thumbnail, obj) => {
+        try {
+            if (fs.existsSync('./desafio.txt')) {
+                const data = JSON.parse(fs.readFileSync('./desafio.txt', 'utf-8'))
+                const lastProd = data[data.length - 1].id
+                this.title = title
+                this.price = price
+                this.thumbnail = thumbnail
+                this.id = lastProd + 1
+                data.push(obj)
+                fs.writeFileSync('./desafio.txt', `${JSON.stringify(data)}`)
+            } else {
+                const array = []
+                this.title = title
+                this.price = price
+                this.thumbnail = thumbnail
+                this.id = 1
+                array.push(obj)
+                fs.writeFileSync('./desafio.txt', `${JSON.stringify(array)}`)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+
+        console.log('Archivo guardado!')
     }
-  }
 
-  async getAll() {
-    try {
-    
-      const data = await fs.promises.readFile(this.archivo, "utf-8");
-      if (data) {
-       
-        this.datos = JSON.parse(data);
-        this.datos.map((producto) => {
-          if (this.id < producto.id) this.id = producto.id;
-        });
-        return this.datos;
-      }
-    } catch (e) {
-      console.log("error: " + e);
+
+    getByID = (id) => {
+        const data = fs.readFileSync('./desafio.txt', 'utf-8')
+        console.log('getById', JSON.parse(data).find(x => x.id === id))
     }
-  }
 
-  async deleteById(id) {
-    /* ------------------------------ Traigo todos ------------------------------ */
-    await this.getAll();
-    try {
-      /* ------------------ filtro todos aquellos q no coincidan ------------------ */
-      const data = this.datos.filter((producto) => producto.id !== id);
-      /* -------- pregunto si se cumple el filtrado, es decir si hay datos -------- */
-      if (data) {
-        /* -------------- entonces escribo en el archivo el nuevo array ------------- */
-        await fs.promises.writeFile(
-          this.archivo,
-          JSON.stringify(data, null, 2)
-        );
-        /* ---------------------------- muestro x consola --------------------------- */
-        console.log(data);
-      }
-    } catch (error) {
-      console.log("Error al eliminar; " + error);
+    getAll = () => {
+        const data = fs.readFileSync('./desafio.txt', 'utf-8')
+        console.log(data)
     }
-  }
 
-  async deleteAll() {
-    try {
-      /* ---------------------Acá elimino el archivo directamente -------------------- */
-      await fs.promises.unlink(this.archivo);
-      console.log("Archivo eliminado");
-    } catch (error) {
-      console.log("Error al elimanr el archivo " + error);
+    deleteById =  (id) => {
+        try {
+            const data = fs.readFileSync('./desafio.txt', 'utf-8')
+            const newArray = JSON.parse(data)
+             const filteredArray = newArray.filter(x => x.id !== id)
+            
+            fs.writeFileSync('./desafio.txt', `${JSON.stringify(filteredArray)}`)
+            console.log('Producto eliminado', filteredArray)
+        } catch (error) {
+            console.log(error)
+        }
     }
-  }
+
+    deleteAll = async () => {
+        await fs.promises.writeFile('./desafio.txt', ``)
+        console.log('Productos eliminados')
+    }
+
+
+
 }
 
-const c = new Contenedor("Producto.txt");
+// Instancio y guardo los productos nuevos
+const contenedor = new Contenedor()
+contenedor.save('Escuadra', 123.5, 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png', contenedor)
+contenedor.save('Calculadora', 234.56, 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png"', contenedor)
+contenedor.save('Globo Terraqueo', 345.67, 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png"',contenedor)
+//Obtengo todos los productos
+// contenedor.getAll()
+//Obtengo el producto con ID 2
+// contenedor.getByID(2)
+//Elimino el producto con ID 2
+contenedor.deleteById(2)
+//Metodo para borrar todo
+// contenedor.deleteAll()
 
-function guardarProducto() {
-  c.save(objProducto1);
-  c.save(objProducto2);
-  c.save(objProducto3);
-}
-
-function ObtenerId(id) {
-  c.getById(id);
-}
-
-async function ObtenerTodos() {
-  console.log(await c.getAll());
-}
-
-function BorrarId(id) {
-  c.deleteById(id);
-}
-function BorrarTodo() {
-  c.deleteAll();
-}
-
-//guardarProducto();
-
-//ObtenerId(1);
-//ObtenerId(2);
-//ObtenerId(3);
-ObtenerTodos ();
-//BorrarId(1);
-//BorrarId(2);
-//BorrarId(3);
-//BorrarTodo();
